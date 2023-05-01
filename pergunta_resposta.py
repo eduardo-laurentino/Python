8c35cb6311050ebe915067d60b3b490ecd743070
Eduardo Laurentino <97697621+eduardo-laurentino@users.noreply.github.com>
2023-04-30 23:02:32 -0300

    commit

---
+++ pergunta_resposta.py
@@ -0,0 +1,43 @@
+perguntas = [
+    {
+        'Pergunta' : 'Quanto é 2 + 2?',
+        'Opções' : ['1', '3', '4', '5'],
+        'Resposta' : '4'
+    },
+    {
+        'Pergunta' : 'Quanto é 3 - 3?',
+        'Opções' : ['1', '3', '0', '5'],
+        'Resposta' : '0'
+    },
+    {
+        'Pergunta' : 'Quanto é 5 * 5?',
+        'Opções' : ['25', '55', '10', '51'],
+        'Resposta' : '25'
+    },
+    {
+        'Pergunta' : 'Quanto é 10 / 2?',
+        'Opções' : ['2', '3', '4', '5'],
+        'Resposta' : '5'
+    },
+]
+
+for pergunta in perguntas:
+    print('Pergunta: ', pergunta['Pergunta'])
+    print()
+    for i, opcao in enumerate(pergunta['Opções']):
+        print(f'{i}) ', opcao)
+    print()
+    escolha_opcao = input("Escolha uma opção: ")
+    acertou = False
+    escolha_int = None
+
+    qtd_opcoes = len(pergunta['Opções'])
+    if escolha_opcao.isdigit():
+        escolha_int = int(escolha_opcao)
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if qtd_opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+    if acertou:
+        print("Acertou!")
+    print()
